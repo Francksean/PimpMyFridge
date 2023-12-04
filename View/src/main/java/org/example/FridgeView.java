79d82225630848ec858f96f42c75b4f6c7950b92
franck sean <seandjissou@gmail.com>
2023-12-04 19:17:26 +0100

    fin des configs avec javaFx

--- View/src/main/java/org/example/FridgeView.java
+++ View/src/main/java/org/example/FridgeView.java
@@ -1,10 +1,16 @@
 package org.example;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+import java.awt.*;
 import java.util.Observable;
 import java.util.Observer;
 
-
-public class FridgeView implements Observer, IFridgeView {
+public class FridgeView extends Application implements Observer, IFridgeView   {
     private IFridgeParams params;
     @Override
     public void update(Observable o, Object params) {
@@ -18,4 +24,12 @@ public class FridgeView implements Observer, IFridgeView {
         this.params = params;
     }
 
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setScene(new Scene(new StackPane(new Text("Hello World")), 300, 300));
+        stage.show();
+    }
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
